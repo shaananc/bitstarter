a03024d10bb10e67de27c56fb0849cbb8d22c126
Shaanan Cohney <shaananc@gmail.com>
2013-07-10 13:56:45 +0000

    homework advance

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
+var Buffer = require('Buffer');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+    var htmlbuf = new Buffer(fs.readFileSync(index.html));
+    response.send(buffer.toString('utf-8');
 });
 
 var port = process.env.PORT || 5000;
