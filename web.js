25cfd7e1f368f9600a69d6aad6bb75b54efee702
Shaanan Cohney <shaananc@gmail.com>
2013-07-10 14:00:56 +0000

    fixed hw

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
     var htmlbuf = new Buffer(fs.readFileSync("index.html"));
-    response.send(buffer.toString('utf-8'));
+    response.send(htmlbuf.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
