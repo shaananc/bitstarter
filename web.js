6d8b49568178f945de3e3a6bd4abf37894bf5d28
Shaanan Cohney <shaananc@gmail.com>
2013-07-18 01:56:36 +0000

    Added CSS and Head Section

--- web.js
+++ web.js
@@ -1,3 +1,4 @@
+#!/usr/bin/node
 var express = require('express');
 var fs = require('fs');
 
@@ -8,7 +9,7 @@ app.get('/', function(request, response) {
     response.send(htmlbuf.toString('utf-8'));
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
