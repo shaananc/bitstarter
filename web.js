e7056a77c993da836634f3542763c0bf134c2513
Shaanan Cohney <shaananc@gmail.com>
2013-08-04 09:51:18 +0000

    fixed web.js

--- web.js
+++ web.js
@@ -1,12 +1,20 @@
 #!/usr/bin/node
 var express = require('express');
 var fs = require('fs');
-
+var htmlfile = "index.html"
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-    var htmlbuf = new Buffer(fs.readFileSync("index.html"));
-    response.send(htmlbuf.toString('utf-8'));
+    var html = fs.readFileSync(htmlfile).toString();
+    response.send(html);
+    //response.send(htmlbuf.toString('utf-8'));
+});
+
+app.get('/img/*.*', function(req, rsp){
+    	var img = fs.readFileSync('img/'+req.params[0]+'.'+req.params[1]);
+	rsp.writeHead(200, {"Content-Type": "image/jpg"});
+	rsp.end(img, "binary");
+	console.log(req);	
 });
 
 var port = process.env.PORT || 8080;
